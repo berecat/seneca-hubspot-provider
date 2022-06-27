f47951460966e213ba96a7a9fc8e2a726f8b6adf
dreemhai <dreemhai.wk@gmail.com>
2022-06-27 13:03:59 +0200

    Update hubspot-provider.ts

--- src/hubspot-provider.ts
+++ src/hubspot-provider.ts
@@ -1,12 +1,12 @@
 const Pkg = require('../package.json');
-const hubspot = require('@hubspot/api-client');
+const Hubspot = require('@hubspot/api-client')
 
 type HubspotProviderOptions = {}
 
 function HubspotProvider(this: any, options : HubspotProviderOptions) {
-	const seneca:any = this;
+	const seneca:any = this
 
-	const entityBuilder = this.export('provider/entityBuilder');
+	const entityBuilder = this.export('provider/entityBuilder')
 
 	seneca.message('sys:provider,provider:hubspot,get:info', get_info)
 	async function get_info(this: any, _msg: any) {
@@ -29,42 +29,42 @@ function HubspotProvider(this: any, options : HubspotProviderOptions) {
 				  cmd: {
 					  list: {
 						  action: async function(this: any, entize: any, msg: any) {
-							  let res, list;
-							  res = (await this.shared.sdk.crm.companies.basicApi.getPage(10)).results;
+							  let res, list
+							  res = (await this.shared.sdk.crm.companies.basicApi.getPage(10)).results
 							  list = res.map((data) => entize(data))
-							  return list;
+							  return list
 						  }
 					  },
 					  load: {
 						  action: async function(this: any, entize: any, msg: any) {
-							  let data = (msg.q.id.split('/'));
-							  let id = data[0];
-							  let obj = {};
-							  data = data.slice(1);
+							  let data = (msg.q.id.split('/'))
+							  let id = data[0]
+							  let obj = {}
+							  data = data.slice(1)
 							  try{
-								  obj = await this.shared.sdk.crm.companies.basicApi.getById(id, data.length != 0 ? data : undefined);
+								  obj = await this.shared.sdk.crm.companies.basicApi.getById(id, data.length != 0 ? data : undefined)
 							  }catch(err){
 								  if(err.code >= 400 && err.code < 500)
-									  return null;
-								  throw err;
+									  return null
+								  throw err
 							  }
-							  return entize(obj);
+							  return entize(obj)
 						  }
 					  },
 					  save: {
 						  action: async function(this: any, entize: any, msg: any) {
-							  let ent = msg.ent;
-							  let id = ent.id;
-							  let desc = ent.properties.description;
-							  let obj;
+							  let ent = msg.ent
+							  let id = ent.id
+							  let desc = ent.properties.description
+							  let obj
 							  try{
-								  obj = await this.shared.sdk.crm.companies.basicApi.update(id, {properties: {description: desc}});
+								  obj = await this.shared.sdk.crm.companies.basicApi.update(id, {properties: {description: desc}})
 							  }catch(err){
 								  if(err.code >= 400 && err.code < 500)
-									  return null;
-								  throw err;
+									  return null
+								  throw err
 							  }
-							  return entize(obj);
+							  return entize(obj)
 						  }
 					  }
 				  }
@@ -77,11 +77,11 @@ function HubspotProvider(this: any, options : HubspotProviderOptions) {
 
 
 	this.prepare(async function(this: any) {
-		let accTok = await this.post('sys:provider,get:key,provider:hubspot,key:accessToken');
+		let accTok = await this.post('sys:provider,get:key,provider:hubspot,key:accessToken')
 		if (!accTok.ok) {
-			this.fail('api-key-missing');
+			this.fail('api-key-missing')
 		 }
-		this.shared.sdk = new hubspot.Client({accessToken: accTok.value});
+		this.shared.sdk = new Hubspot.Client({accessToken: accTok.value})
 	})
 
 	return{
