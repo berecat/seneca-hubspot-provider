c487fd2db985a61268a25debfebb58bf9b24849a
dreemhai <dreemhai.wk@gmail.com>
2022-06-30 19:33:48 +0200

    Update hubspot-provider.test.ts

--- test/hubspot-provider.test.ts
+++ test/hubspot-provider.test.ts
@@ -15,47 +15,46 @@ if (Fs.existsSync(__dirname + '/local-config.js')) {
 }
 
 describe('hubspot-provider', () => {
-	test('happy', async ()=>{
-		expect(HubspotProvider).toBeDefined();
-		expect(HubspotProviderDoc).toBeDefined();
-		const seneca = await makeSeneca();
+  test('happy', async ()=>{
+    expect(HubspotProvider).toBeDefined();
+    expect(HubspotProviderDoc).toBeDefined();
+    const seneca = await makeSeneca();
 
-		let sdk = seneca.export('HubspotProvider/sdk')()
-		expect(sdk).toBeDefined()
+    let sdk = seneca.export('HubspotProvider/sdk')()
+    expect(sdk).toBeDefined()
 		
-		expect(await seneca.post('sys:provider,provider:hubspot,get:info')).toMatchObject({
-			ok: true,
-			name: 'hubspot',
-		})
+    expect(await seneca.post('sys:provider,provider:hubspot,get:info')).toMatchObject({
+      ok: true,
+      name: 'hubspot',
+     })
+  });
 
-	});
+  test('messages', async () => {
+    const seneca = await makeSeneca()
+    await (SenecaMsgTest(seneca, BasicMessages)())
+  })
 
-	test('messages', async () => {
-		const seneca = await makeSeneca()
-		await (SenecaMsgTest(seneca, BasicMessages)())
-	})
+  test('company-basic', async () => {
+    if(!Config) return;
+    const seneca = await makeSeneca();
 
-	test('company-basic', async () => {
-		if(!Config) return;
-		const seneca = await makeSeneca();
+    const list = await seneca.entity('provider/hubspot/company').list$();
+    expect(list.length > 0).toBeTruthy();
 
-		const list = await seneca.entity('provider/hubspot/company').list$();
-		expect(list.length > 0).toBeTruthy();
+    const company0 = await seneca.entity('provider/hubspot/company')
+		    .load$(Config.company0.id);
+    expect(company0.properties.name).toEqual('MyCompany');
 
-		const company0 = await seneca.entity('provider/hubspot/company')
-				.load$(Config.company0.id);
-		expect(company0.properties.name).toEqual('MyCompany');
+    company0.properties.description = "Value: " + Math.random();
+    let company0r = await company0.save$();
+    expect(company0r.id).toEqual(company0.id);
+    expect(company0r.properties.description).toEqual(company0.properties.description);
 
-		company0.properties.description = "Value: " + Math.random();
-		let company0r = await company0.save$();
-		expect(company0r.id).toEqual(company0.id);
-		expect(company0r.properties.description).toEqual(company0.properties.description);
-
-		const company0u = await seneca.entity("provider/hubspot/company")
-						.load$(Config.company0.id)
-    		expect(company0u.properties.name).toEqual('MyCompany');
-    		expect(company0u.properties.description).toEqual(company0u.properties.description);
-	});
+    const company0u = await seneca.entity("provider/hubspot/company")
+				    .load$(Config.company0.id)
+    expect(company0u.properties.name).toEqual('MyCompany');
+    expect(company0u.properties.description).toEqual(company0u.properties.description);
+ });
 });
 
 async function makeSeneca() {
