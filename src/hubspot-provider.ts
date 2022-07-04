508ad13eef18954545d767109fa3ea3f0381f3ba
dreemhai <dreemhai.wk@gmail.com>
2022-07-04 14:16:52 +0200

    Update hubspot-provider.ts

--- src/hubspot-provider.ts
+++ src/hubspot-provider.ts
@@ -11,6 +11,7 @@ function HubspotProvider(this: any, options : HubspotProviderOptions) {
   const entityBuilder = this.export('provider/entityBuilder')
 
   seneca.message('sys:provider,provider:hubspot,get:info', get_info)
+  
   async function get_info(this: any, _msg: any) {
     return {
       ok: true,
@@ -22,72 +23,73 @@ function HubspotProvider(this: any, options : HubspotProviderOptions) {
       }
     }
   }
- entityBuilder(this, {
-   provider: {
-     name: 'hubspot'
-   },
-   entity: {
-     company: {
-       cmd: {
-         list: {
-	     action: async function(this: any, entize: any, msg: any) {
-	       const limit = msg.q.limit || 10 // The maximum number of results to display per page
-	       let res = (await this.shared.sdk.crm.companies.basicApi.getPage(limit)).results
-	       let list = res.map((data) => entize(data))
-	       return list
-	     }
-	 },
+  entityBuilder(this, {
+    provider: {
+      name: 'hubspot'
+    },
+    entity: {
+      company: {
+        cmd: {
+          list: {
+	    action: async function(this: any, entize: any, msg: any) {
+	      const limit = msg.q.limit$ || 10 // The maximum number of results to display per page
+	      let res = (await this.shared.sdk.crm.companies.basicApi.getPage(limit)).results
+	      let list = res.map((data) => entize(data))
+	      return list
+	    }
+	  },
 
-	 load: {
-	   action: async function(this: any, entize: any, msg: any) {
-	     let idParts = msg.q.fields$
-	     let id = msg.q.id
-	     let obj = await this.shared.sdk.crm.companies.basicApi.getById(id, idParts) // docs for the usage: https://developers.hubspot.com/docs/api/crm/companies
-	     return entize(obj)
-	   }
-	 },
+	  load: {
+	    action: async function(this: any, entize: any, msg: any) {
+	      let idParts = msg.q.fields$
+	      let id = msg.q.id
+	      // docs for the usage: https://developers.hubspot.com/docs/api/crm/companies
+	      let obj = await this.shared.sdk.crm.companies.basicApi.getById(id, idParts)
+	      return entize(obj)
+	    }
+	  },
 
-	 save: {
-	   action: async function(this: any, entize: any, msg: any) {
-	     let ent = msg.ent
-	     let id = ent.id
-	     let desc = ent.properties.description
-	     let obj
-	     try{
-	       obj = await this.shared.sdk.crm.companies.basicApi.update(id, {properties: {description: desc}})
-	     }
-	     catch(err){
-	       if(err.code >= 400 && err.code < 500){
-		 return null
-	       }
-	       throw err
-	     }
-	     return entize(obj)
-	   }
-	 }
-       }
-     }
-   }
- })
-
-
-
+	  save: {
+	    action: async function(this: any, entize: any, msg: any) {
+	      let ent = msg.ent
+	      let id = ent.id
+	      let desc = ent.properties.description
+	      let obj
+	      try{
+	        obj = await this.shared.sdk.crm.companies.basicApi.update(id, {properties: {description: desc}})
+	      }
+	      catch(err){
+	        if(err.code >= 400 && err.code < 500){
+		  return null
+	        }
+	        throw err
+	      }
+	      return entize(obj)
+	    }
+	  }
+        }
+      }
+    }
+  })
 
 
- this.prepare(async function(this: any) {
-   let accTok = await this.post('sys:provider,get:key,provider:hubspot,key:accessToken')
-   if (!accTok.ok) {
-     this.fail('api-key-missing')
-   }
-   this.shared.sdk = new Hubspot.Client({accessToken: accTok.value})
- })
 
- return{
-   exports: {
-     sdk: () => this.shared.sdk
-   }
- }
-} 
+
+
+  this.prepare(async function(this: any) {
+    let accTok = await this.post('sys:provider,get:key,provider:hubspot,key:accessToken')
+    if (!accTok.ok) {
+      this.fail('api-key-missing')
+    }
+    this.shared.sdk = new Hubspot.Client({accessToken: accTok.value})
+  })
+
+  return{
+    exports: {
+      sdk: () => this.shared.sdk
+    }
+  }
+}
 
 const defaults : HubspotProviderOptions = {
   debug: false
