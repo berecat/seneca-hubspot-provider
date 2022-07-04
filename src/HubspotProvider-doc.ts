3bdd93a9c106163e8e1fdcb8591bcfa23540ffbf
dreemhai <dreemhai.wk@gmail.com>
2022-07-04 14:07:02 +0200

    Fix the spaces

--- src/HubspotProvider-doc.ts
+++ src/HubspotProvider-doc.ts
@@ -1,12 +1,11 @@
 const docs = {
-	get_info: {
-		desc: 'Get information about the company.',
-	},
+  get_info: {
+    desc: 'Get information about the company.',
+  },
 }
-
 
 export default docs
 
-if('undefined' !== typeof(module)){
-	module.exports = docs
+if ('undefined' !== typeof(module)) {
+  module.exports = docs
 }
